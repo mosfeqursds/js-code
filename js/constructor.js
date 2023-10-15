15e329015bc9c826937f6cc9363c642118a3d7d3
mosfeqursds <108667180+mosfeqursds@users.noreply.github.com>
2023-10-15 09:54:09 +0600

    add: constructor, class ,function and map

--- js/constructor.js
+++ js/constructor.js
@@ -1,14 +1,20 @@
 //  A constructor is a special function that creates and initializes an object instance of a class.
 // Constructor function for Person objects
-function Person(first, last, age, eye) {
+function Person(first, last, year, eye) {
   this.firstName = first;
   this.lastName = last;
-  this.age = age;
+  this.age = year;
   this.eyeColor = eye;
 }
 
 // Create a Person object
-const myInfo = new Person("blue", " ",50, "blue");
+const myInfo = new Person("Mosfequr", " Rahman", 50, "Black");
 
 // Display age
-document.getElementById("demo").innerHTML = "My info is " + myInfo.age + ".";
+document.getElementById("demo").innerHTML =
+  "My first Name is " + myInfo.firstName + ".";
+document.getElementById("demo1").innerHTML =
+  "My last Name " + myInfo.lastName + ".";
+document.getElementById("demo2").innerHTML = "My age is " + myInfo.age + ".";
+document.getElementById("demo3").innerHTML =
+  "My eyeColor is " + myInfo.eyeColor + ".";
---
+++ js/function.js
@@ -0,0 +1,13 @@
+function myFunction(num) {
+  return num * 10;
+}
+
+function sumNumber() {
+  var x = 5;
+  var y = 6;
+  var z = x + y;
+  console.log(z);
+}
+sumNumber();
+sumNumber();
+sumNumber();
