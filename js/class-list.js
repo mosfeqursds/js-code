1d7d920042a7a0787691488c8652bf5c8c630d7d
mosfeqursds <108667180+mosfeqursds@users.noreply.github.com>
2023-10-18 15:18:26 +0600

    add: add or remove class

---
+++ js/class-list.js
@@ -0,0 +1,9 @@
+function classListFunction() {
+  const list = document.getElementById("myDIV").classList;
+  list.add("myStyle");
+}
+
+function removeClassListFunction() {
+  const list = document.getElementById("myDIV").classList;
+  list.remove("myStyle");
+}
